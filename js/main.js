3c3905da518aca39b04c287c89eae1b91c125c33
Remina Greenfield <reminagreenfield@gmail.com>
2015-03-30 18:17:07 -0400

    finished up to boy hugging tree

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+	console.log("window loaded")
+	var s = skrollr.init({
+		render: function(data) {
+		  //Log the current scroll position.
+		  $('#info').text(data.curTop);
+		}
+	});
+})
+
